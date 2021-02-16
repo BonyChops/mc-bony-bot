0a671eb3a6e532054d9dab9b6eacdb29ce15d128
BonyChops <office.kentarou@gmail.com>
2021-02-16 14:50:32 +0900

    Init

---
+++ app.js
@@ -0,0 +1,174 @@
+const mineflayer = require('mineflayer')
+const fs = require('fs');
+const cron = require("node-cron");
+const config = JSON.parse(fs.readFileSync("config.json"));
+const bot = mineflayer.createBot({
+    host: config.host,
+    port: 25565,
+    username: config.username,
+    //username: "Bony_Bot", // email and password are required only for
+    password: config.password,
+    version: false,
+    auth: 'mojang'
+})
+
+bot.on('chat', function (username, message) {
+    if (username === bot.username) return
+    console.log(message)
+})
+
+// Log errors and kick reasons:
+bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
+bot.on('error', err => console.log(err))
+
+bot.once('spawn', () => {
+    // mineflayerViewer(bot, { port: 3007, firstPerson: false })
+})
+let mcData
+bot.once('inject_allowed', () => {
+    mcData = require('minecraft-data')(bot.version)
+})
+
+bot.on('experience', () => {
+    console.log(`I am level ${bot.experience.level}`)
+})
+
+bot.on('chat', (username, message) => {
+    const command = message.split(' ');
+    if (username === bot.username) return
+    switch (true) {
+        case /^list$/.test(message):
+            sayItems()
+            break
+        case /^chest$/.test(message):
+            watchChest(false, ['chest', 'ender_chest', 'trapped_chest'])
+            break
+        case /^sleep$/.test(message): {
+            sleep();
+            break;
+        }
+
+        case /^wake$/.test(message): {
+            bot.wake();
+        }
+    }
+})
+
+cron.schedule("* * * * * *", () => {
+    if (bot.time.timeOfDay > 12541 && bot.time.timeOfDay < 23458 && !bot.isSleeping) {
+        sleep();
+    }
+})
+
+
+
+const sleep = () => {
+    const blocks = ["white_bed"];
+    bedToSleep = bot.findBlock({
+        matching: blocks.map(name => mcData.blocksByName[name].id),
+        maxDistance: 6
+    })
+    bot.sleep(bedToSleep).then(() => {
+        bot.chat("寝ます (ｽﾔｧ")
+    }).catch(e => {
+        bot.chat("無理でした: " + e.toString())
+    })
+}
+
+function sayItems(items = bot.inventory.items()) {
+    const output = items.map(itemToString).join(', ')
+    if (output) {
+        console.log(output)
+    } else {
+        console.log('empty')
+    }
+}
+
+async function watchChest(minecart = false, blocks = []) {
+    let chestToOpen
+
+    chestToOpen = bot.findBlock({
+        matching: blocks.map(name => mcData.blocksByName[name].id),
+        maxDistance: 6
+    })
+    if (!chestToOpen) {
+        console.log('no chest found')
+        return
+    }
+    const chest = await bot.openContainer(chestToOpen)
+    console.log(chest.containerItems())
+    let wait = false;
+    chest.on('updateSlot', async (slot, oldItem, newItem) => {
+        const count = chest.containerItems().filter(item => item.type == 579).reduce((acc, item) => (acc + item.count), 0);
+        console.log(`chest update: ${itemToString(oldItem)} -> ${itemToString(newItem)} (slot: ${slot})`)
+        console.log(count)
+
+        if (!(count >= 9) || wait) {
+            //bot.chat("no more ingod then 9");
+        } else {
+            console.log("job");
+            await chest.withdraw(mcData.itemsByName.iron_ingot.id, null, 9);
+            await chest.close();
+            job(chestToOpen);
+        }
+    })
+    chest.on('close', () => {
+        console.log('chest closed')
+    })
+}
+
+const job = async (chestToOpen) => {
+    wait = true;
+
+    //bot.chat("took diamond");
+    tableToOpen = bot.findBlock({
+        matching: mcData.blocksByName.crafting_table.id,
+        maxDistance: 6
+    })
+    if (!tableToOpen) {
+        console.log('no craft table found')
+        return
+    }
+    const recipe = bot.recipesFor(mcData.itemsByName.iron_block.id, null, 1, tableToOpen)[0]
+    if (!recipe) {
+        bot.chat("error: cant make");
+        return;
+    }
+    console.log("ok");
+    await bot.craft(recipe, 1, tableToOpen)
+    const chest2 = await bot.openContainer(chestToOpen)
+
+    await chest2.deposit(mcData.itemsByName.iron_block.id, null, 1);
+    console.log("job done");
+    await chest2.close();
+    watchChest(false, ["chest"]);
+    return;
+}
+
+function itemToString(item) {
+    if (item) {
+        return `${item.name} x ${item.count}`
+    } else {
+        return '(nothing)'
+    }
+}
+
+function itemByType(items, type) {
+    let item
+    let i
+    for (i = 0; i < items.length; ++i) {
+        item = items[i]
+        if (item && item.type === type) return item
+    }
+    return null
+}
+
+function itemByName(items, name) {
+    let item
+    let i
+    for (i = 0; i < items.length; ++i) {
+        item = items[i]
+        if (item && item.name === name) return item
+    }
+    return null
+}
